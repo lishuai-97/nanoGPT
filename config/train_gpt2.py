ac21a142967a7063e713d6b8c221cab693d41f90
lishuai-97 <lishuai_math@163.com>
2024-12-07 18:29:33 +0800

    Update train_gpt2 124M script

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -2,24 +2,31 @@
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
-wandb_log = True
+exp_name = 'gpt2-124M-lr-1e-3'  # should be same as the run_gpt2.sh script and be changed for each new experiment
+
+wandb_log = False
 wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_run_name=exp_name
+
+tensorboard_log = True
+tensorboard_run_name=exp_name
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
+batch_size = 60
 block_size = 1024
-gradient_accumulation_steps = 5 * 8
+gradient_accumulation_steps = 8
 
 # this makes total number of tokens be 300B
+learning_rate = 1e-3
 max_iters = 600000
 lr_decay_iters = 600000
+min_lr = 1e-4
 
 # eval stuff
 eval_interval = 1000
 eval_iters = 200
-log_interval = 10
+log_interval = 1
 
 # weight decay
 weight_decay = 1e-1
